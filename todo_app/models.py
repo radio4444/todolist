2379bf4eb622f62c39949a0f53a11a81b753790e
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-06-04 13:15:24 -0400

    Change the database name: db -> todoItem Created todo_app model. Make and apply migrations.

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class TodoItem(models.Model):
+	PRIORITY_CHOICES = {
+		'low': 'Low',
+		'med': 'Medium',
+		'high': 'High'
+	}
+	STATUS_CHOICES = {
+		'not started': 'Not started',
+		'in progress': 'In progress',
+		'complete': 'Complete'
+	}
+
+	task = models.CharField(max_length=200)
+	description = models.CharField(max_length=400, blank=True)
+	priority = models.CharField(max_length=4, choices=PRIORITY_CHOICES)
+	deadline = models.DateTimeField()
+	status = models.CharField(max_length=11, choices=STATUS_CHOICES)
+	created = models.DateTimeField(auto_now_add=True)
+	edited = models.DateTimeField(auto_now=True)
