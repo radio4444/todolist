44bc58c321ac10e695338eb0d15e2d19eed8c717
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-06-19 05:44:28 -0400

    Refactor PriorityChoice and StatusChoices keys to use capitalized names for consistency in front-end.

--- todo_app/models.py
+++ todo_app/models.py
@@ -4,19 +4,19 @@ from django.db import models
 # Create your models here.
 class TaskModel(models.Model):
 	PRIORITY_CHOICES = {
-		'low': 'Low',
-		'med': 'Medium',
-		'high': 'High'
+		'Low': 'Low',
+		'Medium': 'Medium',
+		'High': 'High'
 	}
 	STATUS_CHOICES = {
-		'not started': 'Not started',
-		'in progress': 'In progress',
-		'complete': 'Complete'
+		'Not started': 'Not started',
+		'In Progress': 'In progress',
+		'Complete': 'Complete'
 	}
 
 	task_name = models.CharField(max_length=200)
 	description = models.CharField(max_length=400, blank=True)
-	priority = models.CharField(max_length=4, choices=PRIORITY_CHOICES)
+	priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES)
 	deadline = models.DateTimeField()
 	status = models.CharField(max_length=11, choices=STATUS_CHOICES)
 	created = models.DateTimeField(auto_now_add=True)
