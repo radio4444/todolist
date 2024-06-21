66396d6f353a6b45d7b236acd2e253f7ead9678f
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-06-21 18:54:59 -0400

    Add comments to views.py and utilize get_object_or_404 in update and delete views
    - Added descriptive comments for each class in views.py to enhance code readability and understanding.
    - Incorporated get_object_or_404 in the update and delete class-based views for robust error handling.
    
    These changes aim to improve code clarity, maintainability, and error handling in the project's views.

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import get_object_or_404
 from .models import TaskModel
 from .forms import TaskModelForm
 from django.views import generic
@@ -7,34 +7,51 @@ from django.urls import reverse_lazy
 
 # Create your views here.
 class ReadTaskView(generic.ListView):
+	# ListView to display all tasks from TaskModel
 	template_name = 'todo_app/home.html'
 	context_object_name = "tasks"
 
 	def get_queryset(self):
+		# Retrieve all tasks from TaskModel
 		return TaskModel.objects.all()
 
 
 class CreateTaskView(generic.CreateView):
+	# CreateView for creating a single task
 	form_class = TaskModelForm
 	template_name = 'todo_app/create_task.html'
 	success_url = reverse_lazy('home')
 
 	def form_valid(self, form):
+		# Save the form data if it is valid
 		form.save()
 		return super().form_valid(form)
 
 
 class UpdateTaskView(generic.UpdateView):
+	# UpdateView for updating a single task
 	model = TaskModel
 	form_class = TaskModelForm
 	template_name = 'todo_app/update_task.html'
 	success_url = reverse_lazy('home')
 
+	def get_object(self, queryset=None):
+		# Use get_object_or_404 to fetch the object based on pk
+		pk = self.kwargs.get('pk')
+		return get_object_or_404(TaskModel, pk=pk)
+
 	def form_valid(self, form):
+		# Save the form data if it is valid
 		form.save()
 		return super().form_valid(form)
 
 
 class DeleteTaskView(generic.DeleteView):
+	# DeleteView for deleting a single task
 	model = TaskModel
 	success_url = reverse_lazy('home')
+
+	def get_object(self, queryset=None):
+		# Use get_object_or_404 to fetch the object based on pk
+		pk = self.kwargs.get('pk')
+		return get_object_or_404(TaskModel, pk=pk)
