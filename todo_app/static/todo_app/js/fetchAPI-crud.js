305f19810c685b5eb03b229e55ab97b3d032cb0b
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-07-01 08:42:01 -0400

    Fetch data from Django REST API and populate table in fetchAPI-crud.js
    - Updated fetchAPI-crud.js to fetch task data from '/api/tasks/' using Django REST API.
    - Removed one set of dummy data arrays as actual data retrieval and population is now handled dynamically.
    - Populated the table rows with retrieved task data excluding the 'id' field.
    
    This update integrates live data retrieval from the backend API, enhancing the functionality of the table display.

--- todo_app/static/todo_app/js/fetchAPI-crud.js
+++ todo_app/static/todo_app/js/fetchAPI-crud.js
@@ -23,8 +23,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const dummyDataArray = ['No task', 'No Description', 'No Priority',
         'No Deadline', 'No Status', 'No Created', 'No Edited'];
 
-    const dummyDataArray2 = ['No task2', 'No Description2', 'No Priority2',
-        'No Deadline2', 'No Status2', 'No Created2', 'No Edited2'];
 
     const rowData1 = document.createElement('tr');
 
@@ -35,17 +33,46 @@ document.addEventListener('DOMContentLoaded', function () {
         rowData1.append(td);
     });
 
-    const rowData2 = document.createElement('tr');
-    dummyDataArray2.forEach(dummyData => {
-        const td = document.createElement('td');
-        td.textContent = dummyData;
-        rowData2.append(td);
-    });
 
     table.append(rowData1);
-    table.append(rowData2);
+
+    // Django Rest endpoint
+    const endpoint = '/api/tasks/';
+
+    // fetch the data using Django REST API
+    fetch(endpoint)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Request Failed');
+
+        }, networkError => console.log(networkError.message))
+        .then(data => {
+            console.table(data);
+
+            data.forEach(item => {
+                const rowData = document.createElement('tr');
+
+                const keys = Object.keys(item);
+                // Loop through each property of the object, except id
+
+                keys.forEach(key => {
+                    if (key !== 'id') {
+                        const td = document.createElement('td');
+                        td.textContent = item[key]
+                        rowData.append(td);
+                    }
+                });
+
+                table.append(rowData);
+            });
+
+
+        });
 
 
     // Append the table to container
     container.append(table);
 });
+
