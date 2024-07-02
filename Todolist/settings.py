00cb9d295bcbe46860771cda00085e759c25e8ee
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-07-02 14:50:41 -0400

    Generate new Django SECRET_KEY and store it in config.py
    - Generated a new Django SECRET_KEY for enhanced security.
    - Stored the new SECRET_KEY in config.py for better configuration management.
    - Updated settings.py to import and use the SECRET_KEY from config.py.
    
    This update improves the security and maintainability of the project configuration.

--- Todolist/settings.py
+++ Todolist/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z&deykfj7_n&gh2g#g%a65!&=ec+r!0y2_awos4r5sb=_0q)k='
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
