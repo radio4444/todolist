4c380795055e33730463b762c78020b2ac05c886
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-07-06 06:02:14 -0400

    Store Django SECRET_KEY in config.py for improved security
    - Moved the SECRET_KEY from settings.py to config.py for better configuration management.
    - Updated settings.py to import the SECRET_KEY from config.py:
      `SECRET_KEY = config.SECRET_KEY` (replacing the hardcoded value).

--- Todolist/settings.py
+++ Todolist/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z&deykfj7_n&gh2g#g%a65!&=ec+r!0y2_awos4r5sb=_0q)k='
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -114,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
